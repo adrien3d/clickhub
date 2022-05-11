949871870084c76a698a6d832928004e5f56db13
Adrien Chapelet <adrien3d@gmail.com>
2022-05-11 23:01:34 +0200

    first commit

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+//https://clickup.com/api
+
+//https://docs.github.com/en/rest/guides/basics-of-authentication
+//https://github.com/google/go-github
+
+//https://docs.github.com/en/developers/apps/building-oauth-apps/authorizing-oauth-apps
+//https://github.com/settings/applications/new
+
+func main() {
+
+}
