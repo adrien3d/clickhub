b7ff7a0a74dac942d2fd504b5e385d1e7abfb321
Adrien Chapelet <adrien3d@gmail.com>
2022-05-21 16:28:17 +0200

    feat: add new types

--- main.go
+++ main.go
@@ -8,6 +8,23 @@ package main
 //https://docs.github.com/en/developers/apps/building-oauth-apps/authorizing-oauth-apps
 //https://github.com/settings/applications/new
 
-func main() {
+func RegisterHook(hooktype string) (err error) {
+	switch hooktype {
+	case "in_todo": // Missing part
+		// When a task is moved in progress, create branch from develop (CU-taskID_TaskTitle)
+		break
+	case "in_testing": //testing
+		// When a task branch is merged in develop, move card to testing
+		break
+	case "in_prod": //complete: Might be missing
+		// When develop ahead is merged in master, move card that have the same sprint number tag to complete
+		break
+	}
+	return nil
+}
 
+func main() {
+	RegisterHook("in_todo")
+	RegisterHook("in_testing")
+	RegisterHook("in_prod")
 }
